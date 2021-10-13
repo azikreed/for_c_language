ad71f45b23b9de8ff341dfe74e304a519844d6c4
azikreed <azikreed@gmail.com>
2021-10-13 15:10:02 +0500

    added lesson 12

---
+++ Lesson_012.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int son, daraja, natija;
+
+  printf("Sonni kiriting: ");
+  scanf("%d", &son);
+  printf("Darajani kiriting: ");
+  scanf("%d", &daraja);
+  natija = pow(son, daraja);
+  printf("Natija: %d", natija);
+
+  return 0;
+}
