0b71dbe072919171a8b59abadecff392b00b7257
azikreed <azikreed@gmail.com>
2021-10-15 19:27:10 +0500

    added lesson 4

---
+++ Bitwise Operator/4.C_program_toset_nth_bit_of_a_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, n, newNum;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    printf("Enter nth bit: ");
+    scanf("%d", &n);
+
+    newNum = (1 << n) | num;
+
+    printf("Bit set successfully.\n\n");
+    printf("Number before setting %d bit: %d (in decimal)\n", n, num);
+    printf("Number after setting %d bit: %d (in decimal)\n", n, newNum);
+
+    return 0;
+}
