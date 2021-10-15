3b3643856fdc80fa42c445e1742b35734902da56
azikreed <azikreed@gmail.com>
2021-10-15 23:40:49 +0500

    added lesson 5

---
+++ Bitwise Operator/5_C_program_to_clear_nth_bit_of_a_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  int num, n, newNum;
+
+  printf("Enter any number: ");
+  scanf("%d", &num);
+
+  printf("Enter nth bit: ");
+  scanf("%d", &n);
+
+  newNum = num & (~(1 << n));
+
+  printf("Bit cleared successfully.\n\n");
+  printf("Number before clearing %d bit: %d (in decimal)\n", n, num);
+  printf("Number after clearing %d bit: %d (in decimal)\n", n, newNum);
+
+  return 0;
+}
