be05a96f8db0a7350e3fce0ebdf2604d96b7db95
azikreed <azikreed@gmail.com>
2021-10-16 16:01:02 +0500

    added lesson 5

---
+++ If else Exercises/5_check_numbers_even_or_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int num;
+
+  printf("Enter any number: ");
+  scanf("%d", &num);
+
+  if(num % 2 == 0){
+    printf("%d is even number.", num);
+  } else {
+    printf("%d is odd number.", num);
+  }
+
+  return 0;
+}
