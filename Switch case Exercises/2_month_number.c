4d39f4e6cf42dcfce25af2dd2424dcba3a090ddc
azikreed <azikreed@gmail.com>
2021-10-19 12:10:37 +0500

    added lesson 2

---
+++ Switch case Exercises/2_month_number.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+int main() {
+  int month;
+
+  printf("Enter month number: ");
+  scanf("%d", &month);
+
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      printf("31 days");
+      break;
+    case 2:
+      printf("28-29 days");
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      printf("30 days");
+      break;
+    default:
+      printf("such a month does not exist");
+  }
+
+
+  return 0;
+}
