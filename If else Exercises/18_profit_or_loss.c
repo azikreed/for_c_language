fc935f604076a057510be2a0dbd3bcd0a1f1aaca
azikreed <azikreed@gmail.com>
2021-10-19 10:01:26 +0500

    added lesson 18

---
+++ If else Exercises/18_profit_or_loss.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main() {
+  int cp, sp, profit;
+
+  printf("Enter cost price: ");
+  scanf("%d", &cp);
+
+  printf("Enter sell price: ");
+  scanf("%d", &sp);
+
+  if(sp > cp){
+    profit = sp - cp;
+    printf("Profit: %d", profit);
+  } else if(sp < cp){
+    profit = cp - sp;
+    printf("Loss: %d", profit);
+  } else {
+    printf("No profit No loss");
+  }
+
+
+
+  return 0;
+}
